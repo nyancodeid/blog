72d8ab75f12bed4b0e56d48e0437c20c4e9cc0d6
Ryan Aunur Rassyid <ryandevstudio@gmail.com>
2020-02-09 20:24:15 +0700

    update: deploy script update

--- deploy.sh
+++ deploy.sh
@@ -19,16 +19,16 @@ git add dist
 echo "Commit message for gh-pages commit?"
 read pagecommit
 
-git commit -m 'deploy: $pagecommit'
+git commit -m "deploy: $pagecommit"
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git subtree push --prefix dist origin gh-pages
 
 git add -A
 
-echo "Commit message for gh-pages commit?"
+echo "Commit message for master commit?"
 read mastercommit
-git commit -m 'update: $mastercommit'
+git commit -m "update: $mastercommit"
 
 # push master 
 git push origin master
