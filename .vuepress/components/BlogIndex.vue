2acd0ed5ad466fb5e4a0f3f7a8701a3a3741721b
Ryan Aunur Rassyid <ryandevstudio@gmail.com>
2020-02-09 20:20:32 +0700

    update: $mastercommit

--- .vuepress/components/BlogIndex.vue
+++ .vuepress/components/BlogIndex.vue
@@ -42,14 +42,9 @@ export default {
       return `${months[fDate.getMonth()]} ${fDate.getDate()}, ${fDate.getFullYear()} ${fDate.getHours()}:${(fDate.getMinutes() > 10) ? fDate.getMinutes() : `0${fDate.getMinutes()}`}`
     },
     formatImageResize(url, width = 200) {
-      return url
+      if (process.env.NODE_ENV !== "production") return url
 
-      // return 'https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy'
-      //   + '?container=focus'
-      //   + '&resize_w=' + width
-      //   + '&resize_h=' + width
-      //   + '&refresh=604800'
-      //   + '&url=' + url;
+      return `https://cdn.statically.io/gh/nyancodeid/blog/gh-pages${url}`
     }
   },
   computed: {
--- deploy.sh
+++ deploy.sh
@@ -14,8 +14,21 @@ echo 'blog.nyandev.id' > CNAME
 
 cd -
 
-git add -A
-git commit -m 'deploy: blog'
+git add dist
+
+echo "Commit message for gh-pages commit?"
+read pagecommit
+
+git commit -m 'deploy: $pagecommit'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git subtree push --prefix dist origin gh-pages
+
+git add -A
+
+echo "Commit message for gh-pages commit?"
+read mastercommit
+git commit -m 'update: $mastercommit'
+
+# push master 
+git push origin master
