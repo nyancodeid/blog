61bd4624ceb53a654a0ce19f83eecc856f6b11e3
Ryan Aunur <ryandevstudio@gmail.com>
2020-03-11 10:43:47 +0700

    update: update

--- .vuepress/components/Gist.vue
+++ .vuepress/components/Gist.vue
@@ -5,44 +5,60 @@
 </template>
 
 <script>
-  export default {
-    name: 'Gist',
-    props: {
-      id: {
-        type: String,
-        required: true
+function fetcher (url) {
+  return new Promise((resolve, reject) => {
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState !== 4) return;
+      if (xhr.status >= 200 && xhr.status < 300) {
+        try {
+          let res = JSON.parse(xhr.responseText)
+          resolve(res)
+        } catch (err) {
+          reject(err)
+        }
       }
-    },
-    data() {
-      return {
-        content: "Loading"
-      }
-    },
-    created () {
-      this.fetchGistData(this.id)
-    },
-    methods: {
-      fetchGistData (id) {
-        const FETCH_URL = `https://cors-anywhere.herokuapp.com/https://gist.github.com/${id}.json`
-        
-        fetch(FETCH_URL, {
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        }).then(res => res.json()).then((data) => {
-          this.content = data.div
+    };
+    xhr.onerror = reject
+    xhr.open('GET', url);
+    xhr.send();
+  })
+}
 
-          let style = document.getElementById('gist-stylesheet')
-          if (!style) {
-            let css = document.createElement("link")
-            css.id = "gist-stylesheet"
-            css.href = data.stylesheet,
-            css.rel = "stylesheet"
+export default {
+  name: 'Gist',
+  props: {
+    id: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      content: "Loading"
+    }
+  },
+  mounted () {
+    this.fetchGistData(this.id)
+  },
+  methods: {
+    fetchGistData (id) {
+      const FETCH_URL = `https://cors-anywhere.herokuapp.com/https://gist.github.com/${id}.json`
+      
+      fetcher(FETCH_URL).then((data) => {
+        this.content = data.div
 
-            document.getElementsByTagName("head")[0].appendChild(css)
-          }
-        });
-      }
+        let style = document.getElementById('gist-stylesheet')
+        if (!style) {
+          let css = document.createElement("link")
+          css.id = "gist-stylesheet"
+          css.href = data.stylesheet,
+          css.rel = "stylesheet"
+
+          document.getElementsByTagName("head")[0].appendChild(css)
+        }
+      });
     }
   }
+}
 </script>
