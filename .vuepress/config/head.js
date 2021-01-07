d668eb784d944449e6942eb108164f69632f95eb
Ryan Aunur <ryandevstudio@gmail.com>
2021-01-07 23:56:05 +0700

    update: optimized part 3

--- .vuepress/config/head.js
+++ .vuepress/config/head.js
@@ -93,11 +93,13 @@ module.exports = [
   }],
   ['link', {
     rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Katibeh&family=Work+Sans:ital,wght@0,400;0,500;1,400&display=swap"
+    href: "https://fonts.googleapis.com/css2?family=Katibeh&family=Work+Sans:ital,wght@0,400;0,500;1,400&display=swap",
+    media: "all"
   }],
   ['link', {
     rel: "stylesheet",
-    href: "https://cdn.statically.io/gh/PrismJS/prism-themes/dddd93a8/themes/prism-material-dark.min.css"
+    href: "https://cdn.statically.io/gh/PrismJS/prism-themes/dddd93a8/themes/prism-material-dark.min.css",
+    media: "all"
   }],
   ['script', {
     src: "https://cdn.statically.io/gh/aFarkas/lazysizes/gh-pages/lazysizes.min.js",
