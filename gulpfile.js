708174cf848e1bd2ef3709b4b5d5e1c492364560
Ryan Aunur <ryandevstudio@gmail.com>
2021-01-23 22:39:51 +0700

    update: fix search icon

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const replacement = require('gulp-replace');
 const debug = require('gulp-debug');
 
 function transpile() {
-  return src(['dist/**/*.html'])
+  return src(['dist/**/*.html', 'dist/assets/css/*.css'])
     .pipe(replacement('/assets/', 'https://cdn.statically.io/gh/nyancodeid/blog/gh-pages/assets/'))
     .pipe(debug({title: 'debug:'}))
     .pipe(dest('dist'))
